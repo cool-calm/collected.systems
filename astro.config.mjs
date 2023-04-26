8930365cfe10fbdd2241da76640600beeee09352
Patrick Smith <pgwsmith@gmail.com>
2023-04-26 13:05:55 +1000

    Add other node polyfills
    Site completely broke after the last commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,25 @@ import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 import mdx from "@astrojs/mdx";
 import deno from '@astrojs/deno';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: deno(),
-	server: {
-		port: 5999,
-	},
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-		mdx()
-	],
-	site: `http://astro.build`,
+  output: 'server',
+  adapter: deno(),
+  server: {
+    port: 5999,
+  },
+  integrations: [
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+    mdx()
+  ],
+  site: `http://astro.build`,
   vite: {
     ssr: {
       external: ["fs"]
@@ -26,6 +29,31 @@ export default defineConfig({
     resolve: {
       alias: {
         punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
+      }
+    },
+    optimizeDeps: {
+      esbuildOptions: {
+        // Node.js global to browser globalThis
+        define: {
+          global: 'globalThis'
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true
+          }),
+          NodeModulesPolyfillPlugin()
+        ]
+      }
+    },
+    build: {
+      rollupOptions: {
+        plugins: [
+          // Enable rollup polyfills plugin
+          // used during production bundling
+          rollupNodePolyFill()
+        ]
       }
     }
   }
